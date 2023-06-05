0cd49a6d9216844c4fe5dc05407331c871e358d4
Andrius <andriuscanis@gmail.com>
2023-06-05 17:02:23 +0300

    user json

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const ulDOM = document.querySelector('ul');
+
+const data = fetch(").then((response) => {
+  return response.json()
+})
+
+//let HTML = '';
+
+//for (const item of data) {
+ // HTML += `<li>${item.id} ${item.text}</li>`;
+//}
+
+//ulDOM.innerHTML = HTML;
